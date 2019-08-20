71777af6de6b2b2b6d16a73642aa492da00a92d1
robertacintra <roo.moony@gmail.com>
2019-08-20 01:07:37 -0300

    Developing engine page

---
+++ assets/data/ajax.js
@@ -0,0 +1,18 @@
+function ajax_get(url, callback) {
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            console.log('responseText:' + xmlhttp.responseText);
+            try {
+                const data = JSON.parse(xmlhttp.responseText);
+            } catch(err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
+        }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
