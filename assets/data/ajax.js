74a6c1c932acb976aeeeeaa03ef7a5694ee383b8
robertacintra <roo.moony@gmail.com>
2019-08-22 15:15:08 -0300

    Finishing Result's page - start of jQuery development

--- assets/data/ajax.js
+++ assets/data/ajax.js
@@ -1,18 +1,16 @@
-function ajax_get(url, callback) {
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            console.log('responseText:' + xmlhttp.responseText);
-            try {
-                const data = JSON.parse(xmlhttp.responseText);
-            } catch(err) {
-                console.log(err.message + " in " + xmlhttp.responseText);
-                return;
-            }
-            callback(data);
-        }
-    };
-
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+function loadData(){
+    $.getJSON("data.json", selectOptions);
 }
+function selectOptions(cars, status){
+    if(status == 'success'){
+            
+        let colors = `color/${json.items[0].label}`
+        
+    }else{
+        $('.mensagem').text("It was not possible to load the request. Please try again.");
+    }
+}
+$(document).ready(function(){
+    loadData();
+    $('#colorName').click(loadData);
+});
