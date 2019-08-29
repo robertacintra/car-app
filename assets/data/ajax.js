09d4406b103f9364337fc6da9f36ddb552d9de4f
robertacintra <roo.moony@gmail.com>
2019-08-29 15:55:08 -0300

    jQuery and actions development start

--- assets/data/ajax.js
+++ assets/data/ajax.js
@@ -1,16 +1,33 @@
 function loadData(){
-    $.getJSON("data.json", selectOptions);
+    $.getJSON("http://127.0.0.1:8080/assets/data/data.json", selectOptions);
 }
 function selectOptions(cars, status){
-    if(status == 'success'){
-            
-        let colors = `color/${json.items[0].label}`
+    if(status === 'success'){
+        let engines = `engine/${cars.data.engine.items[0].kwh}!`
+        let colors = `color/${cars.data.wheels.items[0].label}!`
+        let price = `${cars.data.price}`
+
+        console.log(engines)
+        console.log(colors)
+        console.log(price)
+
+        // cars.data.wheels.items.forEach( (e, index) => {
+        //     let $div = $('<div>', {"class": `teste${index}`}) 
+        //     $div.text(e.label)
+        //     $('#container').append($div)
+        // })
+
+        cars.data.engine.items.forEach( (e, index) => {
+            let $div = $('<div>', {"class": `${index}`}) 
+            $div.text(e.kwh)
+            $('#containerEngine').append($div)
+        })
         
     }else{
-        $('.mensagem').text("It was not possible to load the request. Please try again.");
+        $('.errormessage').text("It was not possible to load the request. Please try again.");
     }
 }
 $(document).ready(function(){
     loadData();
-    $('#colorName').click(loadData);
+    $('#containerEngine').click(loadData);
 });
